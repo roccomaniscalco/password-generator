741c8481db60302b73e616c0ef4e1c8a39c93098
Rocco <roccomaniscalco2001@gmail.com>
2020-09-22 05:30:33 -0400

    removed password object and its boolean vars; use if-statements to addselected alphabets to alphabetsArr

--- script.js
+++ script.js
@@ -2,105 +2,98 @@
 var generateBtn = document.querySelector("#generate");
 
 // Script global variables
-var password = {
-  passLength: 0,
-  includeLowercase: false,
-  includeUppercase: false,
-  includeNumbers: false,
-  includeSpecialCharacters: false,
-};
-var alphabetsArr = [
-  [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ],
-  [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ],
-  [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-  [
-    "!",
-    "#",
-    "$",
-    "%",
-    "&",
-    "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "[",
-    "\\",
-    "]",
-    "^",
-    "_",
-    "`",
-    "{",
-    "|",
-    "}",
-    "~",
-    '"',
-  ],
-];
+var passLength = 0;
+var alphabetsArr = [];
+var lowerLettersArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+]
+var upperLettersArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+]
+var numbersArr = [1,2,3,4,5,6,7,8,9,0]
+var specialCharactersArr = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  '"'
+]
 
 // Write password to the #password input
 function writePassword() {
@@ -114,12 +107,13 @@ function generatePassword() {
   var pass = "";
   var alphabet = [];
   var alphabetItem = 0;
+  alphabetsArr = [];
 
   promptUser();
 
   /* While pass is less than passLength, 
      append alphabetsArr[random array][random array item] to pass */
-  while (pass.length < password.passLength) {
+  while (pass.length < passLength) {
     alphabet = alphabetsArr[Math.floor(Math.random() * alphabetsArr.length)];
     alphabetItem = alphabet[Math.floor(Math.random() * alphabet.length)];
     pass += String(alphabetItem);
@@ -129,46 +123,44 @@ function generatePassword() {
   return pass;
 }
 
-// Prompt user input
+// Prompt user input and add alphabets to alphabetsArr accordingly
 function promptUser() {
-  password.passLength = prompt(
+  passLength = prompt(
     "How many characters would you like your password to consist of?"
   );
 
   // Prompt password length
   while (
-    password.passLength < 8 ||
-    password.passLength > 250 ||
-    isNaN(parseInt(password.passLength))
+    passLength < 8 ||
+    passLength > 250 ||
+    isNaN(parseInt(passLength))
   ) {
-    password.passLength = prompt(
+    passLength = prompt(
       "'" +
-        password.passLength +
+        passLength +
         "' is not a valid input. Please enter an integer in the range [8,250]."
     );
   }
 
   // Confirm inclusion of lowercase letters
-  password.includeLowercase = confirm(
+  if(confirm(
     "Would you like your password to include lowercase letters?"
-  );
+  )){alphabetsArr.push(lowerLettersArr)}
 
   // Confirm inclusion of uppercase letters
-  password.includeUppercase = confirm(
+  if(confirm(
     "Would you like your password to include uppercase letters?"
-  );
+  )){alphabetsArr.push(upperLettersArr)}
 
   // Confirm inclusion of integers
-  password.includeNumbers = confirm(
+  if(confirm(
     "Would you like your password to include numbers?"
-  );
+  )){alphabetsArr.push(numbersArr)}
 
   // Confirm inclusion of special characters
-  password.includeSpecialCharacters = confirm(
+  if(confirm(
     "Would you like your password to include special characters?"
-  );
-
-  console.log(password);
+  )){alphabetsArr.push(specialCharactersArr)}
 }
 
 // Add event listener to generate button
