6e81a9ad1c042172f6d97775b5cd32d365e107b7
Rocco <roccomaniscalco2001@gmail.com>
2020-09-22 03:47:56 -0400

    created array of uppercase and lowercase leters, numbers, and special characters

--- script.js
+++ script.js
@@ -10,6 +10,96 @@ var password = {
   includeNumbers: false,
   includeSpecialCharacters: false,
 };
+var lowerLetterArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperLetterArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var intArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacterArr = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  '"',
+];
 
 // Write password to the #password input
 function writePassword() {
@@ -25,8 +115,8 @@ function generatePassword() {
 
 // Prompt user input
 function promptUser() {
-  password.passLength = (
-    prompt("How many characters would you like your password to consist of?")
+  password.passLength = prompt(
+    "How many characters would you like your password to consist of?"
   );
 
   // Prompt password length
